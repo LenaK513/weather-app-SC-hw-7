4e0d01add39bc797cc96954058b68b48871372ae
Olena <kryvtsovaolena@gmail.com>
2022-08-13 19:50:00 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,30 +28,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast-weather");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Thue", "Wen", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
  
   <div class="col-2">
-    <div class="forecast-weather__day">${day}</div>
-
+    <div class="forecast-weather__day">${formatDay(forecastDay.dt)}</div>
+    
     <img
-      src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+      src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
       alt="rain"
       width="40"
     />
     <div class="forecast-weather__temperature">
-      <span class="forecast-weather__max"> 28 </span>
-      <span class="forecast-weather__min"> 18 </span>
-    </div>
-  </div>
-`;
+      <span class="forecast-weather__max">${Math.round(
+        forecastDay.temp.max
+      )}</span>
+        <span class="forecast-weather__min">${Math.round(
+          forecastDay.temp.min
+        )}</span>
+          </div>
+          </div>
+          `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
